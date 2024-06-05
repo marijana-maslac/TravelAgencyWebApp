7a005308b445959283a1542e1e4973648f267b12
Marijana <marijananana98@gmail.com>
2024-06-05 22:03:41 +0200

    Connect
    Got travel destination id, didn't connect travel listing yet. Added some style to travel form and main nav

--- travelagency-app/src/app/travel/DataTable.tsx
+++ travelagency-app/src/app/travel/DataTable.tsx
@@ -1,30 +1,40 @@
-import { TravelDestination } from "@prisma/client";
 import React from "react";
+import Link from "next/link";
+import { TravelDestination } from "@prisma/client";
+import "@/styles/travel.css";
 
 interface Props {
   trips: TravelDestination[];
 }
 
 const DataTable = ({ trips }: Props) => {
-  //console.log(trips);
   return (
-    <div>
-      <h2>Travel Destinations</h2>
-      <table>
+    <div className="travel-container">
+      <h2 className="travel-heading">Travel Destinations</h2>
+      <table className="travel-table">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
+            <th className="travel-highlight">ID</th>
+            <th className="travel-highlight">Name</th>
+            <th className="travel-highlight">Description</th>
+            <th className="travel-highlight">Actions</th>
           </tr>
         </thead>
         <tbody>
           {trips
             ? trips.map((trip) => (
                 <tr key={trip.id} data-href="/">
-                  <td>{trip.id}</td>
-                  <td>{trip.name}</td>
-                  <td>{trip.description}</td>
+                  <td className="travel-text">{trip.id}</td>
+                  <td className="travel-text">{trip.name}</td>
+                  <td className="travel-text">{trip.description}</td>
+                  <td>
+                    <Link
+                      className="travel-highlight"
+                      href={`/travel/${trip.id}`}
+                    >
+                      Show Listings
+                    </Link>
+                  </td>
                 </tr>
               ))
             : null}
