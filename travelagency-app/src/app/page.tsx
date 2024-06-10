db64e9cb160e99ede7ae69591660c9e222594b5a
Marijana <marijananana98@gmail.com>
2024-06-10 11:08:01 +0200

    Added style to about and gallery on home page
    Added style to about and gallery on home page. Created slider for images in gallery.

--- travelagency-app/src/app/page.tsx
+++ travelagency-app/src/app/page.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "@/app/page.module.css";
 import Link from "next/link";
 import DataTable from "./travel/DataTable";
 import { TravelListing } from "@prisma/client";
-
+import "@/styles/about.css";
 interface FlexItemProps {
   title: string;
 }
+
 interface Props {
   trips: TravelListing[];
 }
+
 const Page = () => {
   return (
     <div>
@@ -24,9 +27,11 @@ const Page = () => {
           </div>
         </section>
         <div className={styles.flexContainer}>
-          <FlexItem title="About" />
           <FlexItem title="Travel" />
-          <FlexItem title="Gallery" />
+          <div className={styles.flexRow}>
+            <FlexItem title="About" />
+            <FlexItem title="Gallery" />
+          </div>
           <FlexItem title="Contact Us" />
         </div>
       </main>
@@ -56,15 +61,7 @@ const FlexItem: React.FC<FlexItemProps> = ({ title }) => {
       content = (
         <>
           <h2>{title}</h2>
-          <div className={styles.imageGallery}>
-            <img src="/images/paris.jpg" alt="Gallery Image 1" />
-            <img src="/images/london.jpg" alt="Gallery Image 2" />
-            <img src="/images/rome.jpeg" alt="Gallery Image 3" />
-            <img src="/images/newyork.jpg" alt="Gallery Image 4" />
-            <img src="/images/trogir.jpg" alt="Gallery Image 5" />
-            <img src="/images/venice.jpg" alt="Gallery Image 6" />
-            <img src="/images/amsterdam.jpg" alt="Gallery Image 7" />
-          </div>
+          <ImageSlider />
         </>
       );
       break;
@@ -74,16 +71,27 @@ const FlexItem: React.FC<FlexItemProps> = ({ title }) => {
           <h2>{title}</h2>
           <div>
             <p className="about-text">
-              Our team of expert travel advisors is here to help you plan your
-              dream vacation from start to finish. With our extensive knowledge
-              and personalized service, we'll create a tailor-made itinerary
-              that suits your interests, budget, and travel style.
-              <br />
-              <br />
+              At <span className="about-highlight">Travel Agency</span>, we're
+              passionate about travel and dedicated to providing our customers
+              with the best possible experiences. Whether you're seeking a
+              relaxing beach getaway, an exciting adventure tour, or a cultural
+              immersion in a far-off land, we've got you covered.
+            </p>
+
+            <p className="about-text">
+              We believe that travel has the power to enrich lives, create
+              memories, and foster connections with people and places around the
+              globe. That's why we're committed to making your travel dreams a
+              reality and ensuring that every journey with{" "}
+              <span className="about-highlight">Travel Agency</span> is an
+              unforgettable experience.
+            </p>
+            <p className="about-text">
               So why wait? Start planning your next adventure today and let us
               take you on the trip of a lifetime!
             </p>
           </div>
+          <br></br>
           <div className={styles.linkContainer}>
             <Link href={`/${title.toLowerCase()}`}>
               <p>Read more</p>
@@ -109,6 +117,47 @@ const FlexItem: React.FC<FlexItemProps> = ({ title }) => {
   }
 
   return <div className={styles.flexItem}>{content}</div>;
+};
+
+const ImageSlider = () => {
+  const images = [
+    "/images/paris.jpg",
+    "/images/london.jpg",
+    "/images/rome.jpeg",
+    "/images/newyork.jpg",
+    "/images/trogir.jpg",
+    "/images/venice.jpg",
+    "/images/amsterdam.jpg",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className={styles.slider}>
+      <button className={styles.prev} onClick={prevSlide}>
+        &#10094;
+      </button>
+      <div className={styles.sliderImage}>
+        <img
+          src={images[currentIndex]}
+          alt={`Gallery Image ${currentIndex + 1}`}
+        />
+      </div>
+      <button className={styles.next} onClick={nextSlide}>
+        &#10095;
+      </button>
+    </div>
+  );
 };
 
 export default Page;
