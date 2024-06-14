4ea8651c9c69435d8c6dc485f861ba9f100061b8
Marijana <marijananana98@gmail.com>
2024-06-14 15:56:13 +0200

    Next-auth api
    Created api for next-auth, connected signin/signout.

--- travelagency-app/src/components/MainNav.tsx
+++ travelagency-app/src/components/MainNav.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
-const MainNav = () => {
+const MainNav = async () => {
+  const session = await getServerSession(options);
+  console.log(session);
   return (
     <div className="main-container">
       <div className="logo-container">
@@ -16,7 +20,11 @@ const MainNav = () => {
         <Link href="/users">USERS</Link>
       </nav>
       <div className="nav-button">
-        <Link href="/users/addUser">Log in</Link>
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
       </div>
     </div>
   );
