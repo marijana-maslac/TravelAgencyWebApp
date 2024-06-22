75e798f2d571674b2728bfdb69ec8706971dbf22
Marijana <marijananana98@gmail.com>
2024-06-22 23:09:29 +0200

    Update part of reservations
    Created update part of reservations.. just update status of reservation without big functions

--- travelagency-app/src/app/api/reservation/[id]/route.ts
+++ travelagency-app/src/app/api/reservation/[id]/route.ts
@@ -31,3 +31,37 @@ export async function GET(request: NextRequest, { params }: Props) {
     return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
 }
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const reservationId = parseInt(params.id, 10);
+  const body = await request.json();
+
+  try {
+    const updatedReservation = await prisma.reservation.update({
+      where: { id: reservationId },
+      data: { ...body },
+    });
+
+    return NextResponse.json(updatedReservation, { status: 200 });
+  } catch (error) {
+    console.error("Server error: ", error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const reservationId = parseInt(params.id, 10);
+  const body = await request.json();
+  const { status } = body;
+
+  try {
+    const updatedReservation = await prisma.reservation.update({
+      where: { id: reservationId },
+      data: { status },
+    });
+
+    return NextResponse.json(updatedReservation, { status: 200 });
+  } catch (error) {
+    console.error("Server error: ", error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
+  }
+}
